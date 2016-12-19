5a70e1e8c60921e1243d71e61fd5baba8be734d2
aimalz <aimalz@nyu.edu>
2016-12-19 17:48:29 -0500

    adding new preprocessing script for corrected data

---
+++ code/AardvarkData.py
@@ -0,0 +1,81 @@
+"""
+Module for handling the data:
+"""
+import pyfits as pf
+import numpy as np
+import util
+from astropy.cosmology import FlatLambdaCDM
+
+def filter():
+    '''
+    generate indices for downsampling of the data
+    '''
+    np.random.seed(12345)
+    N = 100000 # hard-coded number for downsampling the galaxies
+    indices = np.random.choice(5000000, N)
+
+    return indices
+
+def load_truth():
+    ''' loads the Aardvark catalog containing the true galaxy properties
+    '''
+    data_file = ''.join([util.dat_dir(),
+            'Aardvark-0.0.fits'])
+
+    return pf.open(data_file)[1].data
+
+def load_observations():
+
+    ''' loads the Aardvark catalog containing the observed galaxy properties
+    '''
+    data_file = ''.join([util.dat_dir(),
+            'Aardvark-0_DECAM.0.fits'])
+
+    return pf.open(data_file)[1].data
+
+def generate_alpha():
+    '''generates additional galaxy properties : Mstar, SSFR, ...
+       and write them into a file
+    '''
+    data_file = ''.join([util.dat_dir(),
+            'Aardvark-0.0.fits'])
+    indices = filter()
+
+    redshifts = pf.open(data_file)[1].data['Z'][indices]
+    coeffs = pf.open(data_file)[1].data['COEFFS'][indices]
+    coeffs = coeffs[redshifts < 2.]
+    coeffs = coeffs.T
+    redshifts = redshifts[redshifts < 2.]
+    nredshifts = len(redshifts)
+
+    specfile =  ''.join([util.dat_dir(),
+            'k_nmf_derived.default.fits'])
+    t = pf.open(specfile)
+    tbdata=t[18].data
+    tmass=t[16].data
+    tmremain=t[17].data
+    tmetallicity=t[18].data
+    tmass300=t[19].data
+    tmass1000=t[20].data
+
+    #initialize arrays
+    b300=np.zeros(nredshifts)
+    b1000=np.zeros(nredshifts)
+    tmp_mass=np.zeros(nredshifts)
+    mets=np.zeros(nredshifts)
+    mass=np.zeros(nredshifts)
+    intsfh=np.zeros(nredshifts)
+
+    cosmology=FlatLambdaCDM(H0=70, Om0=0.286)
+    dmod=cosmology.distmod(redshifts)
+    # do all the calculations, looping over objects
+    for i in np.arange(nredshifts):
+        b300[i]=np.sum(tmass300*coeffs[:,i])/np.sum(tmass*coeffs[:,i])
+        b1000[i]=np.sum(tmass1000*coeffs[:,i])/np.sum(tmass*coeffs[:,i])
+        tmp_mass[i]=np.sum(tmremain*coeffs[:,i])
+        mets[i]=np.sum(tmremain*tmetallicity*coeffs[:,i])/tmp_mass[i]
+        mass[i]=tmp_mass[i]*10.**(0.4*dmod.value[i])
+        intsfh[i]=np.sum(coeffs[:,i])*10.**(0.4*dmod.value[i])
+    galprop =  np.rec.fromarrays(np.vstack([b300, b1000, tmp_mass, mets, mass, intsfh]), names = ["b300", "b1000", "tmp_mass", "mets", "mass", "intsfh"])
+
+    return galprop
---
+++ code/AardvarkPreprocessing.py
@@ -0,0 +1,100 @@
+import george
+from george.kernels import ExpKernel , WhiteKernel
+import numpy as np
+from sklearn.preprocessing import RobustScaler
+from scipy.stats import beta
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_context("notebook", font_scale=1.5, rc={"lines.linewidth": 2.5})
+from matplotlib import gridspec
+
+import AardvarkData as data
+import util
+
+RS = RobustScaler()
+
+def data_vectors():
+
+    """load the additional galaxy properties"""
+
+    galprops = data.generate_alpha()
+
+    """load the data vectors"""
+
+    inds = data.filter()
+    true = data.load_truth()
+    obs = data.load_observations()
+
+    """ obsevations """
+    g = obs["TMAG"][:,0]
+    r = obs["TMAG"][:,1]
+    i = obs["TMAG"][:,2]
+    z = obs["TMAG"][:,3]
+    y = obs["TMAG"][:,4]
+
+    """ errors """
+
+    dm = obs["DELTAM"]
+
+    ge = obs["OMAGERR"][:,0]
+    re = obs["OMAGERR"][:,1]
+    ie = obs["OMAGERR"][:,2]
+    ze = obs["OMAGERR"][:,3]
+    ye = obs["OMAGERR"][:,4]
+
+    """ inferrables """
+
+    Z = true['Z']
+    R = true['SIZE']
+    COEFFS = true['COEFFS']
+        
+    CAT = np.vstack([g-r,r-i,i-z,z-y,r,dm,Z,R]).T
+    CATERR = np.vstack([ge,re,ie,ze,ye]).T
+    CATERR = np.log(CATERR)
+
+    """ filtering the catalogs """
+
+    CATERR = CATERR[inds, :]
+    zcut = np.where((CAT[inds,-2] < 2.))[0]
+    CATERR = CATERR[zcut]
+
+    CAT = CAT[inds, :]
+    CAT = CAT[CAT[:,-2] < 2. , :]
+
+
+    """ some magnitudes are higher than 30, let's get rid of them """
+
+    #safe_mags = np.where((CAT[:,0] < 30)&(CAT[:,1] < 30)&(CAT[:,2] < 30)&(CAT[:,3] < 30)&(galprops["mass"]<10**13.3))[0] #(CAT[:,3] < 30)and(CAT[:,4] < 30)and(CAT[:,5] < 30))[0]
+    #CAT = CAT[safe_mags]
+    #galprops = galprops[safe_mags]
+    #CATERR = CATERR[safe_mags]
+
+    """ putting together the final catalog before regression """
+
+    gr, ri, iz, zy, r, dm, Z , R = CAT.T
+
+    mass , sf = galprops["mass"], galprops["b300"]
+
+    masterX = np.vstack([gr, ri, iz, zy, r, dm]).T
+    masterY = np.vstack([Z, np.log10(mass), R]).T
+
+    length = masterX.shape[0]
+    train_ind = np.random.choice(length , int(0.5 * length))
+    test_ind = np.setdiff1d(np.arange(length) , train_ind)
+
+    #np.savetxt(util.dat_dir()+"Xerr_train.dat", CATERR[train_ind])
+    #np.savetxt(util.dat_dir()+"Xerr_test.dat", CATERR[test_ind])
+    np.savetxt(util.dat_dir()+"X_train_A.dat", masterX[train_ind])
+    np.savetxt(util.dat_dir()+"X_test_A.dat", masterX[test_ind])
+    np.savetxt(util.dat_dir()+"COEFFS_train_A.dat", COEFFS[train_ind])
+    np.savetxt(util.dat_dir()+"COEFFS_test_A.dat", COEFFS[test_ind])
+    np.savetxt(util.dat_dir()+"Y_train_A.dat", masterY[train_ind])
+    np.savetxt(util.dat_dir()+"Y_test_A.dat", masterY[test_ind])
+
+    return None
+
+
+if __name__ == '__main__':
+
+   data_vectors()
